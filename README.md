f43e7957cd960740cd9b6acf1ebec98664682e0c
Cyross Makoto <takuan45189@gmail.com>
2023-01-31 12:33:27 +0900

    VegasScriptPrescribedPatternからプロジェクトを移動し、VegasScriptDebugに改名

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # VegasScriptHelper
-VEGASのスクリプトを実装するために便利なクラスライブラリ
+
+VEGASのスクリプトを実装するために便利なクラスライブラリと
+デバッグ用スクリプトとのセット
+
+## VegasScriptHelperのインストールに関して
+
+本番で使用する際はVegasScriptDebug.dllは必要ございません。
+VegasScriptHelper.dllのみ、VEGASの拡張機能フォルダにコピーしてください。
+
+## VegasScriptHelper.dllの本番利用
+
+本ファイルを以下の2箇所、もしくはどちらかにコピーしてください。
+
+- 拡張機能を利用する場合
+  - c:\Users\(ユーザ名)\Documents\Vegas Application Extensions
+- スクリプト機能を利用する場合
+  - c:\Users\(ユーザ名)\Documents\Vegas Script Menu
---
+++ VegasScriptDebug/CustomModule.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+using ScriptPortal.Vegas;
+using VegasScriptHelper;
+
+namespace VegasScriptPrescribedPattern
+{
+    public class CustomModule : ICustomCommandModule
+    {
+        private VegasHelper helper;
+        private readonly static string CommandName = "ShowTrackLength";
+        private readonly static string DisplayName = "トラック中の全イベントの長さを表示";
+        private readonly static string DockName = "全イベントの長さ";
+        private readonly CustomCommand myCommand = new CustomCommand(CommandCategory.Tools, CommandName);
+
+        public void InitializeModule(Vegas vegas)
+        {
+            helper = VegasHelper.Instance(vegas);
+        }
+
+        public ICollection GetCustomCommands()
+        {
+            myCommand.DisplayName = DisplayName;
+            myCommand.Invoked += HandleInvoked;
+            myCommand.MenuPopup += HandleMenuPopup;
+            return new CustomCommand[] { myCommand };
+        }
+
+        void HandleInvoked(Object sender, EventArgs e)
+        {
+            if(!helper.ActivateDockView(DockName))
+            {
+                try
+                {
+                    long nanos = helper.GetLengthFromAllEventsInTrack() + 500000 / 1000000 * 1000000;
+                    TimeSpan span = new TimeSpan(nanos);
+                    string result = string.Format("長さ: {0}", span.ToString("g"));
+
+                    DockableControl dock = new DockableControl(DockName);
+                    Label label1 = new Label
+                    {
+                        Dock = DockStyle.Fill,
+                        Text = result,
+                        TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+                    };
+                    dock.Controls.Add(label1);
+                    helper.LoadDockView(dock);
+                }
+                catch (VegasHelperTrackUnselectedException)
+                {
+                    MessageBox.Show("ビデオトラックが選択されていません。");
+                }
+                catch (VegasHelperNoneEventsException)
+                {
+                    MessageBox.Show("選択したビデオトラック中にイベントが存在していません。");
+                }
+            }
+        }
+
+        void HandleMenuPopup(Object sender, EventArgs e)
+        {
+            myCommand.Checked = helper.FindDockView(DockName);
+        }
+    }
+}
