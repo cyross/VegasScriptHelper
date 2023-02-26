1a4305ba80f5bc463c05b0f771bfe859138324d0
Cyross Makoto <takuan45189@gmail.com>
2023-02-26 10:41:02 +0900

    デプロイのコマンドラインオプション処理を見直し、Markdownファイルコピーの追加

--- deploy_dll_files.py
+++ deploy_dll_files.py
@@ -12,74 +12,129 @@ CONFIG = {}
 
 OPTIONS = {}
 
-OPTION_DEFAULTS = {
-    'update_yaml': False
+OPTION_DEFAULTS: dict[str, any] = {
+    'update_yaml': False,
+    'update_markdown': False,
+    'varbose': False,
+    'test': 0
 }
 
-OPTION_SWITCHES = {
-    'update_yaml': ['-Y','--UPDATE_YAML']
+OPTION_SWITCHES: dict[str, list[str]] = {
+    'update_yaml': ['-Y','--UPDATE_YAML'],
+    'update_markdown': ['-M','--UPDATE_MARKDOWN'],
+    'varbose': ['-V','--varbose'],
     }
 
-def abort_program():
+OPTION_PARAMETERS: dict[str, dict[str, any]] = {
+    'test': { 'option': ['-T','--TEST'], 'type': int }
+    }
+
+def abort_program() -> None:
     print('スクリプトを終了します')
     exit()
 
-def update_option_params(options, option_params):
-    if len(option_params) > 0:
-        param_key = option_params.pop(0)
-        options[param_key] = option_params if len(option_params) > 0 else True
-        print(f'UPDATE OPTION: {param_key}, VALUE: {options[param_key]}')
+def print_varbose(message: str) -> None:
+    if OPTIONS['varbose']:
+        print(message)
+
+def load_config() -> dict[str, any]:
+    config_filepath: str = './deploy_dll_files.yaml'
+
+    if not os.path.exists(config_filepath):
+        print(f'[ERROR]ファイルが見つかりません: {config_filepath}')
+        abort_program()
+
+    try:
+        with open(config_filepath, 'r', encoding='utf-8') as config_file:
+            config: dict[str, any] = yaml.safe_load(config_file)
+    except yaml.YAMLError as e:
+        print('[ERROR]コンフィグファイル読み込み中にエラーが発生しました')
+        print(f'[ERROR][file]{config_filepath}')
+        print(f'[ERROR][message]{e}')
+        abort_program()
+
+    return config
+
+def analyze_option_parameter(args: list[str], index: int, value_type: type) -> any:
+    if index == len(args)-1:
+        print('[WARN]オプションに値がありません: {args[index]}')
+        abort_program()
+
+    try:
+        value = value_type(args[index + 1])
+    except ValueError as e:
+        print('[ERROR]オプションの値が指定の型に変換できません')
+        print(f'[ERROR]option: {args[index]} value: {args[index + 1]}')
+        abort_program()
+
+    return value
 
 def analyze_commandline_options(command_line_args: list[str]) -> dict[str, list[str] | bool]:
     # オプション未指定のときはデフォルト値を使用
     if len(command_line_args) < 2:
         return OPTION_DEFAULTS.copy()
 
-    options = {}
-
+    options: dict[any] = {}
     args: list[str] = command_line_args[1:]
+    next_pass: bool = False
 
-    option_params = []
+    for i in range(len(args)):
+        # このコマンドライン引数はオプションの値のためスルー
+        if next_pass:
+            next_pass = False
+            continue
 
-    for arg in args:
+        arg: str = args[i]
         upper_arg: str = arg.upper()
+        found: bool = False
 
+        # スイッチ型オプションを検索
         for key in OPTION_SWITCHES.keys():
-            if upper_arg in OPTION_SWITCHES[key]:
-                update_option_params(options, option_params)
- 
-                option_params = [key]
-            elif len(option_params) == 0:
-                print(f'[WARN]Illegal option : {arg}')
-            else:
-                option_params.append[arg]
+            found = upper_arg in OPTION_SWITCHES[key]
+            if found:
+                options[key] = True
+                break
 
-    update_option_params(options, option_params)
+        # パラメータ型オプションを検索
+        if not found:
+            for key in OPTION_PARAMETERS.keys():
+                param_info: dict[str, any] = OPTION_PARAMETERS[key]
+                found = upper_arg in param_info['option']
+                if found:
+                    options[key] = analyze_option_parameter(args, i, param_info['type'])
+                    next_pass = True
+                    break
 
-    if len(options.keys()) > 0:
-        print('')
-    
+        if not found:
+            print(f'[WARN]存在しないオプションです : {arg}')
+
     # 指定していないものはデフォルト値で
     result_options = OPTION_DEFAULTS.copy()
     result_options.update(options)
 
     return result_options
 
-def copy_file_to_folder(filename, file_dir, folders, doc_path, is_yaml = False, update_yaml = False):
+def copy_file_to_folder(
+    filename: str,
+    file_dir: str,
+    folders: list[str],
+    doc_path: str,
+    is_yaml: bool = False,
+    update_yaml: bool = False) -> None:
     # コピーする条件:
     #   - DLLファイル: 無条件
     #   - YAMLファイル: 新規書き込みか、update_yamlオプション指定時
-    src_file_path = os.path.join(file_dir, filename)
+    src_file_path: str = os.path.join(file_dir, filename)
     for dst_dir_base in folders:
-        dst_dir = dst_dir_base.format(doc_path)
-        dst_file_path = os.path.join(dst_dir, filename)
+        dst_dir: str = dst_dir_base.format(doc_path)
+        dst_file_path: str = os.path.join(dst_dir, filename)
         if not is_yaml or (is_yaml and (not os.path.exists(dst_file_path) or update_yaml)):
             copy_file(src_file_path, dst_file_path)
         else:
-            print(f'not copy : {filename}')
+            print(f'[NOTICE]not copy : {filename}')
 
-def copy_file(src_path, dst_path):
-    print(f'copy {src_path}\n  -> {dst_path}')
+def copy_file(src_path: str, dst_path: str):
+    print_varbose(f'copy {src_path}\n  -> {dst_path}')
     try:
         shutil.copyfile(src_path, dst_path)
     except FileNotFoundError:
@@ -88,80 +143,67 @@ def copy_file(src_path, dst_path):
     except shutil.SameFileError:
         print(f'[ERROR]コピー元とコピー先のファイルパスが同じです: {src_path} -> {dst_path}')
         abort_program()
-    except Exception as e:
-        print(f'[ERROR]エラーが発生しました: {e}')
-        abort_program()
+
+def deploy_files(dst_file_folder: str, vegas_script_files: list[str], my_documents_path: str) -> None:
+    # VEGAS HELPER
+    for dst_file_folder in dst_file_folders:
+        copy_file_to_folder(
+            CONFIG['vegas_helper_file_name'],
+            CONFIG['vegas_helper_file_dir'],
+            dst_file_folder,
+            my_documents_path
+            )
+
+    # VEGAS SCRIPT
+    for file_info in vegas_script_files:
+        copy_file_to_folder(
+            file_info['file'],
+            file_info['dir'],
+            dst_file_folder,
+            my_documents_path
+            )
+
+    # YAML FILES
+    for file_info in CONFIG['vegas_yaml_files']:
+       copy_file_to_folder(
+           file_info['file'],
+           file_info['dir'],
+           dst_file_folder,
+           my_documents_path,
+           True,
+           OPTIONS['update_yaml'])
+
+    # MarkDown FILES
+    for file_info in CONFIG['vegas_markdown_files']:
+        copy_file_to_folder(
+            file_info['file'],
+            file_info['dir'],
+            dst_file_folder,
+            my_documents_path,
+            True,
+            OPTIONS['update_markdown'])
 
 if __name__ == '__main__':
-    print('start deploy...\n')
+    print('start deploy...')
 
-    with open('./deploy_dll_files.yaml', 'r', encoding='utf-8') as yaml_file:
-        CONFIG = yaml.safe_load(yaml_file)
+    CONFIG: dict[str, any] = load_config()
 
-    print('analyze command line options...\n')
+    OPTIONS: dict[str, any] = analyze_commandline_options(sys.argv)
 
-    OPTIONS = analyze_commandline_options(sys.argv)
+    print_varbose(f'options = {OPTIONS}')
 
-    print(f'options = {OPTIONS}')
-
-    print('get my documents path\n')
-
-    my_documents_path = os.getenv('MY_DOCUMENTS_PATH')
+    my_documents_path: str = os.getenv('MY_DOCUMENTS_PATH')
 
     if my_documents_path is None:
         print('[NOTICE]MY_DOCUMENTS_PATH環境変数の設定が必須です')
         print('[例]C:\\Users\\Hoge\\Documents\\vegas_extension_files -> MY_DOCUMENTS_PATH="C:\\Users\\Hoge\\Documents"')
         quit()
 
-    # VEGAS HELPER
-    copy_file_to_folder(
-        CONFIG['vegas_helper_file_name'],
-        CONFIG['vegas_helper_file_dir'],
-        CONFIG['dst_vegas_script_folder'],
-        my_documents_path
-        )
-    copy_file_to_folder(
-        CONFIG['vegas_helper_file_name'],
-        CONFIG['vegas_helper_file_dir'],
-        CONFIG['dst_vegas_extension_folder'],
-        my_documents_path
-        )
+    dst_file_folders: list[list[str]] = [CONFIG['dst_vegas_script_folder'], CONFIG['dst_vegas_extension_folder']]
 
-    # VEGAS SCRIPT
-    for file_info in CONFIG['vegas_script_files']:
-        copy_file_to_folder(
-            file_info['file'],
-            file_info['dir'],
-            CONFIG['dst_vegas_script_folder'],
-            my_documents_path
-            )
+    print('copy to vegas script folder...')
+    deploy_files(CONFIG['dst_vegas_script_folder'], CONFIG['vegas_script_files'], my_documents_path)
+    print('copy to vegas application extension folder...')
+    deploy_files(CONFIG['dst_vegas_extension_folder'], CONFIG['vegas_extension_files'], my_documents_path)
 
-    # VEGAS APPLICATION EXTENSION
-    for file_info in CONFIG['vegas_extension_files']:
-        copy_file_to_folder(
-            file_info['file'],
-            file_info['dir'],
-            CONFIG['dst_vegas_extension_folder'],
-            my_documents_path
-            )
-
-    # YAML FILES
-    for yaml_file_name in CONFIG['vegas_yaml_files']:
-        copy_file_to_folder(
-            yaml_file_name,
-            CONFIG['vegas_yaml_file_dir'],
-            CONFIG['dst_vegas_script_folder'],
-            my_documents_path,
-            True,
-            OPTIONS['update_yaml'])
-
-    for yaml_file_name in CONFIG['vegas_yaml_files']:
-        copy_file_to_folder(
-            yaml_file_name,
-            CONFIG['vegas_yaml_file_dir'],
-            CONFIG['dst_vegas_extension_folder'],
-            my_documents_path,
-            True,
-            OPTIONS['update_yaml'])
-
-    print('\ncomplete!')
+    print('complete!')
